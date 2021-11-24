ecf8351afd69fba69191feadaa0a98c0ce2c73b3
peteechu <peteandrecadelina@gmail.com>
2021-11-24 14:08:55 +0800

    fix

---
+++ lib/services/GoogleSignIn.dart
@@ -0,0 +1,18 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+
+// class GoogleSignin extends ChangeNotifier{
+//   final googleSignIn = GoogleSignin();
+
+//   GoogleSignInAccount? _user;
+
+//   GoogleSignInAccount get user => _user;
+
+//   Future<UserCredential> googleLogin() async {
+    
+
+//   }
+
+
+// }
